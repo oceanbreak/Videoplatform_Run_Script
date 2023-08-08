ed4ac2dd277ecef1b77f26b10ac330d9297121ca
Oceanbreak <oceanbreak@gmail.com>
2023-08-08 13:50:54 +0000

    Buffer Concatinates Data as a class

--- lib/data/BufferGenerator.py
+++ lib/data/BufferGenerator.py
@@ -1,12 +1,13 @@
 from lib.data.SonarThread import SonarThread
 from lib.data.ComPortData import ComPortData
-from lib.data.DataStructure import ComPortString
+from lib.data.DataStructure import ComPortString, data_keywords
+from lib.folder_struct.Settings import ComPortSettings
 import time
 print("__generate_buffer__\n")
 
 
 class BufferGenerator:
-    def __init__(self, com_port, rate, messages, data_keywords, printout=False):
+    def __init__(self, com_port, rate, messages, data_keywords, printout=False, IO=False):
         self.__printout = printout
         self.data_keyword = data_keywords
         self._com_port = com_port
@@ -14,6 +15,7 @@ class BufferGenerator:
         self._messages = messages
         self._out_message = False
         self._cur_data = None
+        self.IO_mode = IO
         try:
             self._data_line = ComPortData(com_port, rate, 5, messages, data_keywords) #CHANGE
         except:
@@ -47,8 +49,8 @@ class BufferGenerator:
             self._cur_data = cur_string
 
 
-    def repeat_writing_buffer(self, IO_mode=False):
-        if not IO_mode:
+    def repeat_writing_buffer(self):
+        if not self.IO_mode:
             self.proc_buffer = SonarThread(self.write_buffer_entry)
         else:
             self.proc_buffer = SonarThread(self.write_buffer_IO_byte_mode)
@@ -85,6 +87,75 @@ class BufferConcatinator:
             ret[bobj.keyword] = bobj.string
         return ret
 
+
+class BufferCollection:
+
+    def __init__(self, settings):
+
+        self.ports = {settings.navi_port : settings.navi_port.port,
+         settings.depth_port : settings.depth_port.port,
+         settings.altimeter_port : settings.altimeter_port.port,
+         settings.temp_port : settings.temp_port.port,
+         settings.inclin_port : settings.inclin_port.port}
+        
+        self.rearanged_buffer_settings = None
+        self.buffer_processes = None
+
+        self.rearange()
+        
+
+    def rearange(self):
+        buffers_settings_list = {}
+
+        # Form dict of buffer settings, in case several messages coming from 1 port
+        for item in self.ports:
+            if self.ports[item] not in buffers_settings_list:
+                if item.enable:
+                    buffers_settings_list[self.ports[item]] = [item]
+            else:
+                if item.enable:
+                    buffers_settings_list[self.ports[item]].append(item)
+
+        self.rearanged_buffer_settings = buffers_settings_list
+        return buffers_settings_list
+    
+
+    def InnitiateBuffers(self):
+        # Start generating buffers
+        buffer_dict = self.rearanged_buffer_settings
+        self.buffer_processes = []
+        for com_name in buffer_dict:
+            cur_kw = []
+            cur_msg = []
+            port = buffer_dict[com_name][0].port
+            rate = buffer_dict[com_name][0].rate
+            for com_setting in buffer_dict[com_name]:
+                cur_kw.append(com_setting.keyword)
+                cur_msg.append(com_setting.message)
+
+            if cur_kw[0] == data_keywords.INCLIN:
+                # Special treatment for inclinometer
+                self.buffer_processes.append(BufferGenerator(com_name, rate, ['\x68\x04\x00\x04\x08'], cur_kw, IO=True))
+            else:
+                self.buffer_processes.append(BufferGenerator(com_name, rate, cur_msg, cur_kw))
+
+        for buffer_proc in self.buffer_processes:
+            buffer_proc.repeat_writing_buffer()
+
+        return self.buffer_processes
+
+
+    def stopWritingBuffers(self):
+        for buffer_proc in self.buffer_processes:
+            buffer_proc.stop_writing_buffer()
+
+    def getRawData(self):
+        buffer_output = []
+        for buffer_proc in self.buffer_processes:
+            buffer_output.append(buffer_proc.getData())
+
+        concatinator = BufferConcatinator(*buffer_output)
+        return concatinator.concatinate()
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,57 +1,70 @@
 from lib.data.ComPortData import ComPortData
-from lib.data.BufferGenerator import BufferGenerator, BufferConcatinator
+from lib.data.BufferGenerator import BufferGenerator, BufferConcatinator, BufferCollection
 import time
-from lib.data.DataStructure import CoordinatesData, DepthData
+from lib.data.DataStructure import CoordinatesData, DepthData, data_keywords
 from lib.data.NmeaParser import NmeaParser
 from lib.folder_struct.Settings import Settings
 
 
+
 global_settings = Settings()
+global_settings.readSettings()
 
-global_settings.navi_port.set_port(13)
-global_settings.navi_port.set_rate(9600)
-global_settings.navi_port.set_message('GGA')
+buffer = BufferCollection(global_settings)
 
-global_settings.depth_port.set_port(13)
-
+buffer_rocess = buffer.InnitiateBuffers()
 
 
 try:
-    a = DepthData(17.642)
-    print(a)
 
-    proc1 = BufferGenerator('COM13', 9600, ['GGA'], ['NAVI'])
-    proc2 = BufferGenerator('COM4', 9600, ['DBS'], ['DEPTH'])
-    proc3 = BufferGenerator('COM10', 57600, ['DBT', 'MTW'], ['ALT', 'TEMP'])
-    proc4 = BufferGenerator('COM3', 9600, ['\x68\x04\x00\x04\x08'], ['INCLIN'])
-    parser = NmeaParser()
+    while True:
 
-    proc1.repeat_writing_buffer()
-    proc2.repeat_writing_buffer()
-    proc3.repeat_writing_buffer()
-    proc4.repeat_writing_buffer(IO_mode=True)
+        # buffer_output = []
+        # for buffer_proc in buffer_rocess:
+        #     buffer_output.append(buffer_proc.getData())
 
-    while(True):
-        b1 = proc1.getData()
-        b2 = proc2.getData()
-        b3 = proc3.getData()
-        b4 = proc4.getData()
+        # concatinator = BufferConcatinator(*buffer_output)
+        # output = concatinator.concatinate()
+        output = buffer.getRawData()
+        print(output)
+        time.sleep(0.1)
+
+finally:
+    buffer.stopWritingBuffers()
+
+# try:
+#     proc1 = BufferGenerator('COM13', 9600, ['GGA'], ['NAVI'])
+#     proc2 = BufferGenerator('COM4', 9600, ['DBS'], ['DEPTH'])
+#     proc3 = BufferGenerator('COM10', 57600, ['DBT', 'MTW'], ['ALT', 'TEMP'])
+#     proc4 = BufferGenerator('COM3', 9600, ['\x68\x04\x00\x04\x08'], ['INCLIN'], IO=True)
+#     parser = NmeaParser()
+
+#     proc1.repeat_writing_buffer()
+#     proc2.repeat_writing_buffer()
+#     proc3.repeat_writing_buffer()
+#     proc4.repeat_writing_buffer()
+
+#     while(True):
+#         b1 = proc1.getData()
+#         b2 = proc2.getData()
+#         b3 = proc3.getData()
+#         b4 = proc4.getData()
 
 
-        bc = BufferConcatinator(b1, b2, b3, b4)
-        buffer_list = bc.concatinate()
-        print(buffer_list)
+#         bc = BufferConcatinator(b1, b2, b3, b4)
+#         buffer_list = bc.concatinate()
+#         print(buffer_list)
         
 
-        time.sleep(1.0)
+#         time.sleep(1.0)
 
-    # for  i in range(10):
-    #     # print(proc1.getData())
-    #     # print(proc1.getData() is None)
-    #     time.sleep(1)
-finally:
-    proc1.stop_writing_buffer()
-    proc2.stop_writing_buffer()
-    proc3.stop_writing_buffer()
-    proc4.stop_writing_buffer()
+#     # for  i in range(10):
+#     #     # print(proc1.getData())
+#     #     # print(proc1.getData() is None)
+#     #     time.sleep(1)
+# finally:
+#     proc1.stop_writing_buffer()
+#     proc2.stop_writing_buffer()
+#     proc3.stop_writing_buffer()
+#     proc4.stop_writing_buffer()
 
