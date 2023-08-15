c4ec39c9f6911a3257b5f58c39b5c0d94b940621
Ivan Anisimov <oceanbreak@gmail.com>
2023-08-15 23:04:12 +0300

    Cleaning up

--- main_test.py
+++
@@ -1,9 +0,0 @@
-from lib.folder_struct.ScanDirectory import ScanDirectory
-import time
-
-dir_scan = ScanDirectory('D:/')
-
-for i in range(100):
-    print(i)
-    dir_scan.getAddedItem()
-    time.sleep(1)
