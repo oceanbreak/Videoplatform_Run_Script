cf0b7f00af76b3334011c99535c5977f352a4eaa
Ivan Anisimov <oceanbreak@gmail.com>
2023-08-20 10:40:44 +0300

    Generate log_name from DateTime Struct

--- lib/folder_struct/LogFileGenerator.py
+++ lib/folder_struct/LogFileGenerator.py
@@ -9,9 +9,9 @@ class LogFileGeneraror:
 
     def __init__(self, file_path : str, data_collection : DataCollection):
         self.path = file_path
+        self.data_collection = data_collection
         self.file_name = self.generateFileName()
         self.file_full_path = os.path.join(self.path, self.file_name)
-        self.data_collection = data_collection
         
         # Write header
         with open(self.file_full_path, 'a') as fs:
@@ -28,7 +28,8 @@ class LogFileGeneraror:
 
 
     def generateFileName(self, prefix = 'log', extension = 'csv'):
-        cur_time = time.gmtime()
-        date_str = '{:0>2}'.format(cur_time.tm_mday) +  '{:0>2}'.format(cur_time.tm_mon)  + '{:0>2}'.format(cur_time.tm_year)[-2:]
-        time_str = '{:0>2}'.format(cur_time.tm_hour) + '{:0>2}'.format(cur_time.tm_min)  + '{:0>2}'.format(cur_time.tm_sec)
-        return prefix + '_' + date_str + '_' + time_str + '.' + extension
+        cur_time = self.data_collection.datetime.data
+        # date_str = '{:0>4}'.format(cur_time.tm_year) +  '{:0>2}'.format(cur_time.tm_mon)  + '{:0>2}'.format(cur_time.tm_mday)[-2:]
+        # time_str = '{:0>2}'.format(cur_time.tm_hour) + '{:0>2}'.format(cur_time.tm_min)  + '{:0>2}'.format(cur_time.tm_sec)
+        datetime_str = f'{cur_time.year}{cur_time.mon:0>2}{cur_time.day:0>2}_{cur_time.hour:0>2}{cur_time.min:0>2}{cur_time.sec:0>2}'
+        return prefix + '_' + datetime_str + '.' + extension
