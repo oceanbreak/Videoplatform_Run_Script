177cd910206474453a997eb881bfa1c774cd9981
Ivan Anisimov <oceanbreak@gmail.com>
2023-08-21 16:42:24 +0300

    Working srt generator

--- lib/folder_struct/SrtFromLog.py
+++ lib/folder_struct/SrtFromLog.py
@@ -9,6 +9,86 @@ from tkinter import filedialog
 from lib.UI.Settings import Settings
 from lib.data.DataCollection import DataCollection
 from lib.data.DataStructure import *
+from lib.folder_struct.SrtGenerator import SrtGenerator
+from lib.data.DataStructure import DateTime
+from datetime import datetime, timedelta
+
+
+class SrtFromLog:
+
+    video_format_list = ['avi', 'AVI', 'mp4', 'MP4']
+
+    def __init__(self, settings : Settings, data_collection : DataCollection):
+        self.data_collection = data_collection
+        self.settings = settings
+        self.folder_path = None
+        self.log_data = None
+
+    def askFolder(self):
+        ret = filedialog.askdirectory(initialdir=self.settings.default_folder)
+        if ret != '':
+            self.folder_path = ret
+        return ret
+
+    def getVideoList(self):
+        self.video_list = [name for name in os.listdir(self.folder_path) 
+                           if name.split('.')[-1] in self.video_format_list
+                            and name[0]=='R']
+        
+    def getLogList(self):
+        self.log_list = [name for name in os.listdir(self.folder_path) if name.split('.')[-1] == 'csv']
+
+
+    def readLogData(self):
+        log_reader = LogReader('', self.data_collection)
+        for log_file in self.log_list:
+            path = os.path.join(self.folder_path, log_file)
+            log_reader.readLogFile(path)
+        self.log_data = log_reader.getLogData()
+        return self.log_data
+    
+
+    def writeSrt(self, video_file_name):
+        srt_data = [] # Initialize SRT data
+        video_full = os.path.join(self.folder_path, video_file_name)
+        srt_gen = SrtGenerator(video_full, self.data_collection)
+        
+        video_timer = VideoTimer(video_file_name)
+        video_timer.parseVideoName()
+        duration = video_timer.videoTimeToSeconds()
+
+        for cur_sec in range(duration):
+            cur_timestamp = video_timer.incrementVideoSecond()
+            print(cur_timestamp.toLogTimestamp())
+            try:
+                srt_data.append(self.log_data[cur_timestamp.toLogTimestamp()].toSrtString())
+            except KeyError:
+                srt_data.append('No data')
+        
+        srt_gen.write_srt_data(srt_data)
+
+    def iterateOverVideos(self):
+        for video_file in self.video_list:
+            print(f'Generating Subtitles for {video_file}')
+            self.writeSrt(video_file)          
+
+    
+    def run(self):
+        ret = self.askFolder()
+        if ret == '':
+            print('No folder specified')
+            return 0
+        try:
+            self.getVideoList()
+            self.getLogList()
+            print(f'Found {len(self.video_list)} videos in folder: ', self.video_list)
+            print(f'Found {len(self.log_list)} log_files in folder: ', self.log_list)
+            self.readLogData()
+            self.iterateOverVideos()
+        except ValueError as e:
+            print('Error occured', e)
+
+        
 
 class LogVariable:
     """
@@ -17,7 +97,6 @@ class LogVariable:
 
     def __init__(self):
         self.value = False
-
 
 
 class LogString:
@@ -173,7 +252,35 @@ class LogReader:
     def getLogData(self):
         return self.log_data
 
+
+
+class VideoTimer:
+
+    def __init__(self, video_file):
+        self.video_file = '.'.join(video_file.split('.')[:-1])
+
+
+    def parseVideoName(self):
+        try:
+            pref, start_date, start_time, end_date, end_time = self.video_file.split('_')
+            self.time_start = TimeStamp().videoBewardStamp(start_date, start_time)
+            self.time_end = TimeStamp().videoBewardStamp(end_date, end_time)
+        except ValueError:
+            print('Invalid video name')
+
+
+    def videoTimeToSeconds(self):
+        self.video_length = TimeStamp.differenceInSec(self.time_start, self.time_end)
+        return self.video_length
+    
+    def incrementVideoSecond(self):
+        # Increment start to 1 second
+        return self.time_start.addSeconds()
+    
+
+
 class TimeStamp:
+    # Class for SrtFromLog
 
     def __init__(self):
         self.year = None
@@ -186,19 +293,20 @@ class TimeStamp:
     def logStamp(self, date, time):
         # Makes data from log time_samp of format
         # YYYY/MM/DD, hh":mm:ss
-        self.year, self.month, self.day = date.split('/')
-        self.hour, self.minute, self.second = time.split(':')
+        self.year, self.month, self.day = map(int, date.split('/'))
+        self.hour, self.minute, self.second = map(int, time.split(':'))
         return self
 
     def videoBewardStamp(self, date, time):
-        self.year = date[0:4]
-        self.month = date[4:6]
-        self.day = date[6:8]
-        self.hour = time[0:2]
-        self.minute = time[2:4]
-        self.second = time[4:6]
+        self.year = int(date[0:4])
+        self.month = int(date[4:6])
+        self.day = int(date[6:8])
+        self.hour = int(time[0:2])
+        self.minute = int(time[2:4])
+        self.second = int(time[4:6])
         return self
         
+
     def __str__(self):
         return f'{self.year}/{self.month}/{self.day} {self.hour}:{self.minute}:{self.second}'
     
@@ -206,159 +314,42 @@ class TimeStamp:
         return self.year == other.year and self.month == other.month and \
                 self.day == other.day and self.hour == other.hour and \
                 self.minute == other.minute and self.second == other.second
-
-class SrtFromLog:
-
-    video_format_list = ['avi', 'AVI', 'mp4', 'MP4']
-
-    def __init__(self, settings : Settings, data_collection : DataCollection):
-        self.data_collection = data_collection
-        self.settings = settings
-        self.folder_path = None
-        self.log_data = None
-        
-
-    def askFolder(self):
-        ret = filedialog.askdirectory(initialdir=self.settings.default_folder)
-        if ret != '':
-            self.folder_path = ret
-        return ret
-
-    def getVideoList(self):
-        self.video_list = [name for name in os.listdir(self.folder_path) 
-                           if name.split('.')[-1] in self.video_format_list
-                            and name[0]=='R']
-        
-    def getLogList(self):
-        self.log_list = [name for name in os.listdir(self.folder_path) if name.split('.')[-1] == 'csv']
-
-
-    def readLogData(self):
-        log_reader = LogReader('', self.data_collection)
-        for log_file in self.log_list:
-            path = os.path.join(self.folder_path, log_file)
-            log_reader.readLogFile(path)
-        self.log_data = log_reader.getLogData()
-        return self.log_data
     
-    def run(self):
-        ret = self.askFolder()
-        if ret == '':
-            print('No folder specified')
-            return 0
-        self.getVideoList()
-        self.getLogList()
-        print(f'Found {len(self.video_list)} videos in folder: ', self.video_list)
-        print(f'Found {len(self.log_list)} log_files in folder: ', self.log_list)
-        self.readLogData()
-        print(self.log_data)
+    def addSeconds(self, secs=1):
+        # Add amount of seconds to current time
+        t1 = datetime(self.year, self.month, self.day, self.hour, self.minute, self.second)
+        t2 = t1 + timedelta(seconds=secs)
+        self.year = t2.year
+        self.month = t2.month
+        self.day = t2.day
+        self.hour  = t2.hour
+        self.minute = t2.minute
+        self.second = t2.second
+        return self
+        
+    
+    def toSeconds(self):
+        # Return stored time in seconds where 0 - begin of the day
+        # Year, month and day ignored
+        return self.second + self.minute * 60 + self.hour * 3600
+    
 
+    def toLogTimestamp(self):
+        return f'{self.year}/{self.month:0>2}/{self.day:0>2}', \
+                f'{self.hour:0>2}:{self.minute:0>2}:{self.second:0>2}'
 
-# Utils functions
-def timeToSec(hhmmss_string):
-    hh, mm, ss = map(int, hhmmss_string.split(':'))
-    return hh * 3600 + mm * 60 + ss
-
-
-def secToTime(srt_timer):
-    # converts number into srt time format
-    return '{:0>2}'.format(srt_timer//3600) + ':' \
-           + '{:0>2}'.format((srt_timer//60) % 60) + ':' \
-           + '{:0>2}'.format(srt_timer % 60)
-
-def log_string_format(log_string):
-    return ' '.join(log_string[1:7]) + \
-            '\nDistance: %s m\nDepth: %s m Altimeter: %s m' % (log_string[0], log_string[7], log_string[9])
-
-
-def getVideoList(path):
-    video_list = [name for name in os.listdir(path) if name.split('.')[-1] == 'avi'
-                  and name[0]=='R']
-    return video_list
-
-
-def getStartEndTime(video_name):
-    """
-    This function takes video name and generates timecode relation
-    between video time and real time from log file
-    :param video_name:
-    :return: dictionary that maps real time to video timecode
-    """
-    start_date = video_name.split('_')[1]
-    start_date = "/".join([start_date[6:], start_date[4:6], start_date[:4]])
-    start_time = video_name.split('_')[2]
-    start_time = ":".join([start_time[:2], start_time[2:4], start_time[4:]])
-    end_date = video_name.split('_')[3]
-    end_date = "/".join([end_date[6:], end_date[4:6], end_date[:4]])
-    end_time = video_name.split('_')[4].split('.')[0]
-    end_time = ":".join([end_time[:2], end_time[2:4], end_time[4:]])
-
-    video_timecode = {}
-    video_length = timeToSec(end_time) - timeToSec(start_time)
-    for cur_sec in range(video_length):
-        cur_key = start_date + '_' + secToTime(timeToSec(start_time) + cur_sec - SEC_SRT_OFFSET)
-        video_timecode[cur_key] = cur_sec
-
-    return video_timecode
-
-
-def readLogFile(path):
-    """
-    Takes log file and splits it into dictionary
-    where key is real time and value is other data
-    :param path:
-    :return:
-    """
-    log_list = [name for name in os.listdir(path) if name.split('.')[-1] == 'csv']
-    log = {}
-    for log_file in log_list:
-        with open(os.path.join(path, log_file), 'r') as log_input:
-            for line in log_input:
-                data = line.rstrip().split(';')
-                date_time = '_'.join(data[-2:])
-                log[date_time] = data[:-2]
-    return log
-
-
-def generateSrt(video_timecode, log):
-    counter = 1
-    srt_data = []
-    for cur_key in video_timecode.keys():
-        # print(cur_key)
-        try:
-            line = log[cur_key]
-            # print(line)
-            data_line = cur_key + '\n' + log_string_format(line) + '\n'
-        except KeyError:
-            data_line = cur_key + '\nNo data here'
-
-        srt_block = str(counter) + '\n' +\
-            secToTime(counter - 1) + ',000 --> ' + secToTime(counter) + ',000\n' +\
-            data_line + '\n'
-        srt_data.append(srt_block)
-        counter += 1
-
-    return srt_data
-
-
-
-
-if __name__ == '__main__':
-
-    path = filedialog.askdirectory(initialdir='D:/VIDEOPLATFORM_REC')
-    SEC_SRT_OFFSET = 1
-
-    v_list = getVideoList(path)
-    log = readLogFile(path)
-
-    for video in v_list:
-        cur_timecode = getStartEndTime(video)
-        srt_name = os.path.join(path, video.split('.')[-2] + '.srt')
-        srt_data = generateSrt(cur_timecode, log)
-        with open(srt_name, 'w') as srt_file:
-            for line in srt_data:
-                srt_file.write(line)
-                srt_file.write('\n')
-        print(srt_name + ' written successful')
+    
+    @staticmethod
+    def differenceInSec(time_begin , time_end):
+        # Check if it's next day
+        if time_end.year > time_begin.year or \
+            time_end.month > time_begin.month or \
+            time_end.day > time_begin.day:
+            addition = 3600 * 24
+        else:
+            addition = 0
+        # Calculate difference of wto  time stamps
+        return addition + time_end.toSeconds() - time_begin.toSeconds()
+    
 
 
